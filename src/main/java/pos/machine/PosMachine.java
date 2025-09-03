cefd505cba792205a8ad546fb4359231c324717b
yyy exile <13092787993@163.com>
2025-09-03 21:42:30 +0800

    add loaddata and validate functions

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,7 +3,14 @@ package pos.machine;
 import java.util.List;
 
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
-        return null;
+    // ========== Step 1. 数据库加载 ==========
+    private Map<String, Item> loadItemDatabase() {
+        return ItemsLoader.loadAllItems().stream().collect(Collectors.toMap(Item::getBarcode, item -> item));
+    }
+
+    // ========== Step 2. 校验 ==========
+    private boolean validateBarcodes(List<String> barcodes) {
+        Set<String> validBarcodes = loadItemDatabase().keySet();
+        return barcodes.stream().allMatch(validBarcodes::contains);
     }
 }
