91adc2c5b858bced5b68dd33532fd6880b7dd7d1
yyy exile <13092787993@163.com>
2025-09-03 21:44:29 +0800

    add countbarcodes and matchitems functions

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -13,4 +13,23 @@ public class PosMachine {
         Set<String> validBarcodes = loadItemDatabase().keySet();
         return barcodes.stream().allMatch(validBarcodes::contains);
     }
+
+    // ========== Step 3. 统计数量 ==========
+    private Map<String, Integer> countBarcodes(List<String> barcodes) {
+        return barcodes.stream()
+                .collect(Collectors.toMap(code -> code, code -> 1, Integer::sum));
+    }
+
+    // ========== Step 4. 匹配商品 ==========
+    private List<ReceiptItem> matchItems(Map<String, Integer> barcodeCount) {
+        Map<String, Item> itemDatabase = loadItemDatabase();
+        return barcodeCount.entrySet()
+                .stream()
+                .map(entry -> {
+                    Item item = itemDatabase.get(entry.getKey());
+                    return new ReceiptItem(item.getName(), entry.getValue(), item.getPrice());
+                })
+                .collect(Collectors.toList());
+    }
+
 }
